e04ad7e9264e20cbb4c5dc6293082496cd2da158
Tor Robinson <torrobinson@gmail.com>
2018-01-28 14:31:53 -0700

    Added unit test project

---
+++ LuxaforLyncTool/LuxaforLyncTool_Test/ClientTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using LuxaforLyncTool_Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LuxaforLyncTool_Test
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void DefaultBrightness_IsValid()
+        {
+            Settings settings = new Settings();
+            Assert.IsTrue(settings.Brightness > 0.0 && settings.Brightness <= 1.0);
+        }
+    }
+}
